405d92a9527888fcb85d352df33051c22344ad03
Fabio Galdo <fabiogaldo@gmail.com>
2025-09-01 00:15:08 -0300

    feat(accounts): auth flow complete, reading domain auth cookie through middleware

--- interface/middleware.ts
+++ interface/middleware.ts
@@ -1,47 +1,55 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { createServerClient } from '@supabase/ssr';
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
 
 export async function middleware(request: NextRequest) {
-  // Cria uma resposta para que possamos manipular os cookies
-  const response = NextResponse.next();
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
 
-  // Cria o cliente Supabase para o servidor
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
+        getAll() {
+          return request.cookies.getAll();
         },
-        set(name: string, value: string, options) {
-          request.cookies.set({ name, value, ...options });
-        },
-        remove(name: string, options) {
-          request.cookies.set({ name, value: '', ...options });
+        setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            request.cookies.set(name, value, options);
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options);
+          });
         },
       },
     }
   );
 
-  // Obtém o usuário da sessão do Supabase
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  // Se o usuário não estiver autenticado, redireciona para a página de login
- if (!user) {
-    // Redireciona para a URL completa da aplicação de autenticação
-    const redirectUrl = process.env.NEXT_PUBLIC_AUTH_URL! + '/signin';
+  const publicPaths = ['/public', '/about', '/contact', '/signin', '/signup'];
+  const isPublicPath = publicPaths.includes(request.nextUrl.pathname);
+  console.log('Session:', session);
+  if (!session && !isPublicPath) {
+
+    const redirectUrl = new URL('/signin', process.env.NEXT_PUBLIC_AUTH_URL).toString();
     return NextResponse.redirect(redirectUrl);
   }
 
-  // Se o usuário estiver autenticado, continua para a rota solicitada
   return response;
 }
 
-// Configura quais rotas serão protegidas pelo middleware
 export const config = {
-  matcher: ['/'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
